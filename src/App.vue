e444c36991724c769f832d8834b082aac184583d
KGrzeg <kupczykgrzeg@gmail.com>
2020-06-22 00:38:24 +0200

    Added number display component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <DisplayNumber :number="200" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import DisplayNumber from "@/components/DisplayNumber.vue";
 
 export default {
   name: "App",
-  components: {
-    HelloWorld
-  }
+  components: { DisplayNumber }
 };
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: border-box;
+}
+
+body {
+  background-color: #111;
+  color: yellowgreen;
+  font-family: "Lucida Console", "Courier New", Courier, monospace;
 }
 </style>
---
+++ src/components/DisplayNumber.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="number">
+    <div class="pair" v-for="pair in pairs" :key="pair">
+      <span class="title">{{pair.title}}</span>
+      <span class="digits">{{pair.digits}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DisplayNumber",
+  props: {
+    number: Number
+  },
+  computed: {
+    pairs() {
+      return [
+        { title: "Decimal", digits: this.number },
+        { title: "Binary", digits: this.number.toString(2) },
+        { title: "Octal", digits: this.number.toString(8) },
+        { title: "Hex", digits: this.number.toString(16) }
+      ];
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.number {
+  display: flex;
+  flex-direction: column;
+  width: 200px;
+  padding: 0.2em;
+  background-color: rgba(124, 188, 10, 1);
+}
+
+.pair {
+  display: flex;
+  flex-direction: row;
+  color: black;
+  line-height: 1.2em;
+
+  background: rgb(157, 213, 58);
+  background: -moz-linear-gradient(
+    top,
+    rgba(157, 213, 58, 1) 0%,
+    rgba(161, 213, 79, 1) 50%,
+    rgba(128, 194, 23, 1) 51%,
+    rgba(124, 188, 10, 1) 100%
+  );
+  background: -webkit-linear-gradient(
+    top,
+    rgba(157, 213, 58, 1) 0%,
+    rgba(161, 213, 79, 1) 50%,
+    rgba(128, 194, 23, 1) 51%,
+    rgba(124, 188, 10, 1) 100%
+  );
+  background: linear-gradient(
+    to bottom,
+    rgba(157, 213, 58, 1) 0%,
+    rgba(161, 213, 79, 1) 50%,
+    rgba(128, 194, 23, 1) 51%,
+    rgba(124, 188, 10, 1) 100%
+  );
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#9dd53a', endColorstr='#7cbc0a',GradientType=0 );
+
+  & > * {
+    width: 50%;
+  }
+}
+
+.digits {
+  border-left: 1px solid rgba(0, 0, 0, 0.2);
+  text-align: right;
+}
+</style>
